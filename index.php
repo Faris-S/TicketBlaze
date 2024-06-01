98e12576282fab8aa43a8a1f10b9dd6e3780499a
Faris-S <faris.selimovich@gmail.com>
2024-06-01 17:38:58 +0200

    bugfix

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'rest/routes/middleware_routes.php';
+require 'rest/routes/tickets_routes.php';
+require 'rest/routes/faq_routes.php';
+require 'rest/routes/news_routes.php';
+require 'rest/routes/servicestatus_routes.php';
+require 'rest/routes/auth_routes.php';
+
+Flight::route("/", function(){
+    Flight::redirect('/index.html');
+});
+
+
+Flight::start();
