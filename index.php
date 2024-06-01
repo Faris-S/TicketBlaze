0543c047f6c0937e64e8678b82386987c7dabdea
Faris-S <faris.selimovich@gmail.com>
2024-06-01 17:57:49 +0200

    bugfix

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
-    // This should be called info.php
-    phpinfo();
-?>
+
+require '/backend/vendor/autoload.php';
+require '/backend/rest/routes/middleware_routes.php';
+require '/backend/rest/routes/tickets_routes.php';
+require '/backend/rest/routes/faq_routes.php';
+require '/backend/rest/routes/news_routes.php';
+require '/backend/rest/routes/servicestatus_routes.php';
+require '/backend/rest/routes/auth_routes.php';
+
+Flight::start();
