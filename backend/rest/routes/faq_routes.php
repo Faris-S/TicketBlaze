4673d9840b4d75c375d96d9d8df5b11159d50607
Faris-S <faris.selimovich@gmail.com>
2024-06-01 18:32:03 +0200

    faq bugfix

--- backend/rest/routes/faq_routes.php
+++ backend/rest/routes/faq_routes.php
@@ -2,9 +2,6 @@
 
 require __DIR__ . "/../services/faqService.class.php";
 
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-
 /**
  * @OA\Get(
  *      path="/faq",
@@ -22,16 +19,6 @@ use Firebase\JWT\Key;
  * )
  */
 Flight::route("GET /faq", function(){
-    try {
-        $token = Flight::request()->getHeader('Authentication');
-        if (!$token){
-            Flight::halt(401, 'Token not provided');
-        }
-        $decoded = JWT::decode($token, new Key(JWT_SECRET, "HS256"));
-    } catch (Exception $e) {
-        Flight::halt(401, $e->getMessage());
-    }
-
     $faqService = new faqService();
     $data = $faqService->get_faq();
     echo json_encode($data);
